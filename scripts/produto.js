1613304120a7934bf21569fbe93b8d2eccf1c217
Eduardo <etavares.dev@gmail.com>
2020-11-24 23:27:17 -0300

    refatorada requisição e integração com a api

--- scripts/produto.js
+++ scripts/produto.js
@@ -12,46 +12,17 @@ function onSubmitFormProduto(evento) {
         descricao: document.forms['formProduto']['descricao'].value
     };
 
-    const reqHttp = new XMLHttpRequest();
-
-    reqHttp.onload = function () {
-        console.log(reqHttp.responseText);
-
+    requisicao(function () {
         adicionarProdutoNaLista(produto);
-    }
-
-    reqHttp.onerror = function () {
-        console.log(reqHttp.responseText);
-    }
-
-    reqHttp.open('POST', 'https://5fb5b78836e2fa00166a48cc.mockapi.io/produtos');
-
-    reqHttp.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
-
-    reqHttp.send(JSON.stringify(produto));
+    }, 'POST', 'cadastrar', produto);
 }
 
 function buscarProdutos() {
-
-    const reqHttp = new XMLHttpRequest();
-
-    reqHttp.onload = function () {
-        const produtos = JSON.parse(reqHttp.responseText);
-
+    requisicao(function (produtos) {
         for (let i = 0, len = produtos.length; i < len; i++) {
             adicionarProdutoNaLista(produtos[i]);
         }
-    }
-
-    reqHttp.onerror = function () {
-        console.log(reqHttp.responseText);
-    }
-
-    reqHttp.open('GET', 'https://5fb5b78836e2fa00166a48cc.mockapi.io/produtos');
-
-    reqHttp.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
-
-    reqHttp.send();
+    }, 'GET', 'listar');
 } 
 
 function adicionarProdutoNaLista(produto) {
@@ -61,4 +32,23 @@ function adicionarProdutoNaLista(produto) {
     item.classList.add("itemProduto");
 
     listaProduto.appendChild(item);
+}
+
+function requisicao (onload, metodo, caminho, dados) {
+    const reqHttp = new XMLHttpRequest();
+
+    reqHttp.onload = function () {
+        onload(JSON.parse(reqHttp.responseText));
+    }
+
+    reqHttp.onerror = function () {
+        console.log(reqHttp.responseText);
+    }
+
+
+    reqHttp.open(metodo, 'http://localhost:19112/api_produto/' + caminho);
+
+    reqHttp.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+
+    reqHttp.send(dados ? JSON.stringify(dados) : null);
 }
